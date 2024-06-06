a5681e2f044b1b710a6154a83450a96bca89e01b
SHEFOO10 <445585913@g102019.moe.edu.eg>
2024-06-07 02:52:18 +0300

    fix pycodestyle errors

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ 5. Complex types - list of floats """
 
+
 def sum_list(input_list: list[float]) -> float:
     """
     sum_list - takes a list of floats and return the sum of them.

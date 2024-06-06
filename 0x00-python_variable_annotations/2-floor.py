de86eb9ae719758f63082c4747a81e288d2d6d9a
SHEFOO10 <445585913@g102019.moe.edu.eg>
2024-06-07 02:30:06 +0300

    2. Basic annotations - floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" 2. Basic annotations - floor """
+import math
+
+
+def floor(n: float) -> int:
+    """
+    floor - takes a float (n) and return the floor of it.
+    n: float
+    Return: int
+    """
+    return int(n)

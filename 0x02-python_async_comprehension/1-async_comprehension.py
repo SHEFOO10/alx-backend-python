97298fccd7c871f212b01c9e9238f57b50e61fcd
Sherif Hamdy <79402373+SHEFOO10@users.noreply.github.com>
2024-06-11 11:12:52 +0000

    fix pycodestyle errors

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ 1. Async Comprehensions """
-async_generator = __import__('0-async_generator').async_generator
 from typing import List
+async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:

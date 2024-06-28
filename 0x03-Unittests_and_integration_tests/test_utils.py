0b0a73a53e765b81e1177d20a67bf28bfebab0b3
Sherif Hamdy <79402373+SHEFOO10@users.noreply.github.com>
2024-06-28 20:12:12 +0000

    3. Parameterize and patch

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -59,4 +59,23 @@ class TestGetJson(unittest.TestCase):
 
 
 class TestMemoize(unittest.TestCase):
-    pass
+    """TestMemoize"""
+    def test_memoize(self) -> None:
+        """ test_memoize """
+        class TestClass:
+
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+        with patch.object(
+            TestClass,
+            'a_method',
+            return_value: lambda: 42,
+        ) as mock_fn:
+            test_class = TestClass()
+            self.assertEqual(test_class.a_property, 42)
+            self.assertEqual(test_class.a_property, 42)
+            mock_fn.assert_called_once()
